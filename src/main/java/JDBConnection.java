c203a2a27d0b98173d5f80d441934550a1b55795
ChrisElliott <chrisae456@gmail.com>
2023-12-18 19:52:45 -0500

    add thing

---
+++ src/main/java/JDBConnection.java
@@ -0,0 +1,6 @@
+public class JDBConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/p2bsdb";
+    private static final String DB_NAME = "root";
+    private static final String DB_PASSWORD = "";
+
+}
