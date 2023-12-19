253b68ef98baa0c87902756927343d00c067eaaa
ChrisElliott <chrisae456@gmail.com>
2023-12-18 21:08:03 -0500

    added implementation

---
+++ src/main/java/BookDao.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public interface BookDao {
+    void addBook (Book book);
+    void updateBook (Book book);
+    void deleteBook (int id);
+    Book getBookById (int bookId);
+    List<Book> getAllBooks ();
+}
--- src/main/java/JDBConnection.java
+++ src/main/java/JDBConnection.java
@@ -1,6 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/p2bsdb";
     private static final String DB_NAME = "root";
     private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection(){
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(DB_URL,DB_NAME,DB_PASSWORD);
+            System.out.println("Database connected successfully.");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 
 }
